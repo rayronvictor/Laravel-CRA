65d4dd3b930b0aa8bfa770be51297a896b2d3047
Rayron Victor <rayronvictor@gmail.com>
2020-03-05 14:10:34 -0300

    Add Laradock

--- server/routes/web.php
+++ server/routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*
+ * Qualquer outra rota vai para app.blade.php
+ */
+Route::get('{any}', function () {
     return view('app');
-});
+}).where(['any' => '.*']);
+
+//Route::get('/', function () {
+//    return view('app');
+//});
