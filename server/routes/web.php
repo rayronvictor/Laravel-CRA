81f1da604f40ab814ec143525a8bd29308f54894
Rayron Victor <rayronvictor@gmail.com>
2020-03-05 17:22:07 -0300

    Configura o .env do servidor para acessar os containers do laradock

--- server/routes/web.php
+++ server/routes/web.php
@@ -18,8 +18,4 @@ use Illuminate\Support\Facades\Route;
  */
 Route::get('{any}', function () {
     return view('app');
-}).where(['any' => '.*']);
-
-//Route::get('/', function () {
-//    return view('app');
-//});
+})->where('any', '.*');
